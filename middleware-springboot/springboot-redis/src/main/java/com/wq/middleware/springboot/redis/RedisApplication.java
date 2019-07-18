f2eb09b5be88b7666c044ee2b63968baf616c420
MichaelPaul0416 <wangqiang20995@hundsun.com>
2019-07-18 17:08:52 +0800

    中间件使用project 初始化

---
+++ middleware-springboot/springboot-redis/src/main/java/com/wq/middleware/springboot/redis/RedisApplication.java
@@ -0,0 +1,30 @@
+package com.wq.middleware.springboot.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @Author: wangqiang20995
+ * @Date:2019/7/18
+ * @Description:
+ * @Resource:
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class RedisApplication {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisApplication.class);
+
+    public static void main(String[] args){
+        ApplicationContext applicationContext = SpringApplication.run(RedisApplication.class,args);
+        if (applicationContext != null){
+            logger.info("spring boot application started...");
+        }
+
+    }
+}
